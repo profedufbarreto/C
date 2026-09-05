3434bc22ec82cc25f5c756e22a22d444bc9933e7
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-05 20:13:00 -0300

    matriz

--- main.c
+++ main.c
@@ -6,15 +6,20 @@ int main(){
     // SetConsoleOutputCP(65001);
     // SetConsoleCP(65001);
 
-    int numeros[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int tab, resultado;
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
 
-    printf("Qual tabuada você quer? ");
-    scanf("%d", &tab);
-    
-    for(int i = 0; i < 10; i++){
-        resultado = tab * numeros[i];
-        printf("O resultado é: %d\n", resultado);
+    printf("Elemento na linha 1, coluna 2: %d\n\n", matriz[1][2]);
+
+    printf("--- Exibindo a Matriz Completa ---\n");
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
