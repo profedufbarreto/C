101d1b8fdeefc4c5147afa6834e253066197885c
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-05 16:25:58 -0300

    tabuada

--- main.c
+++ main.c
@@ -7,9 +7,14 @@ int main(){
     // SetConsoleCP(65001);
 
     int numeros[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int tab, resultado;
 
-    for(int i = 0; i <= 9; i++){
-        printf("%d\n", numeros[i]);
+    printf("Qual tabuada você quer? ");
+    scanf("%d", &tab);
+    
+    for(int i = 0; i < 10; i++){
+        resultado = tab * numeros[i];
+        printf("O resultado é: %d\n", resultado);
     }
 
     return 0;
