d4ae03a4f910abfb55b5428f86efb706ab12ae09
profefbarreto <01289211027@senacrs.edu.br>
2026-09-05 11:23:34 -0300

    soma

--- main.c
+++ main.c
@@ -6,7 +6,17 @@ int main(){
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    printf("Olá mundo!");
+    int num1, num2, soma;
+
+    printf("Digite um valor para num1: ");
+    scanf("%d", &num1);
+
+    printf("Digite um valor para num2: ");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+
+    printf("O resultado da soma é: %d\n", soma);
 
     return 0;
 }
