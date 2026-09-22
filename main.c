53f463160a69e43690ca5ecd763e00a2d0562b5a
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-22 17:32:43 -0300

    função

--- main.c
+++ main.c
@@ -2,18 +2,11 @@
 #include<locale.h>
 //#include<windows.h>
 
-void somar(int a, int b){
-    int resultado;
-    printf("\nDigite um valor para a: ");
-    scanf("%d", &a);
-    printf("\nDigite um valor para b: ");
-    scanf("%d", &b);
-    resultado = a + b;
-    printf("O resultado é: %d\n", resultado);
-}
-
-int soma(int a, int b){
-    return a + b;
+void numeros(){
+    int num[] = {1, 2, 3, 4, 5,};
+    for(int i = 0; i < 5; i++){
+        printf("%d\n", num[i]);
+    }
 }
 
 int main(){
@@ -23,14 +16,7 @@ int main(){
     //SetConsoleOutputCP(65001);
     //SetConsoleCP(65001);
 
-    int num1, num2;
-    int resultado1;
-
-    somar(num1, num2);
-
-    resultado1 = soma(4, 2);
-    
-    printf("%d\n", resultado1);
+   numeros();
 
     return 0;
 }
