15dcdbfda2c86421d57f2dbf5cb3284d0054a1dd
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-22 16:53:32 -0300

    função

--- main.c
+++ main.c
@@ -1,27 +1,19 @@
 #include<stdio.h>
 #include<locale.h>
-#include<windows.h>
+//#include<windows.h>
 
+void saudar(){
+    printf("Olá mundo!!\n");
+}
 
 int main(){
 
     // setlocale(LC_ALL, "pt_BR.UTF8");
-    // setlocale(LC_ALL, "Portuguese_Brazil.1252");
-    SetConsoleOutputCP(65001);
-    SetConsoleCP(65001);
+     setlocale(LC_ALL, "Portuguese_Brazil.1252");
+    //SetConsoleOutputCP(65001);
+    //SetConsoleCP(65001);
 
-    int contador;
-    
-
-    printf("Digite um valor para ser o contador: ");
-    scanf("%d", &contador);
-
-    while(contador >= 0){
-        contador--;
-        printf("\n%d", contador);
-    }
-    
-    printf("\nBOoomMmMmMm");
+    saudar();
 
     return 0;
 }
