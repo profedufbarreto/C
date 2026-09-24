07cfbfccc2b77663d38dffc66b0bff717561aebb
profefbarreto <01289211027@senacrs.edu.br>
2026-09-24 16:16:29 -0300

    while

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 #include<locale.h>
-//#include<windows.h>
-
-void numeros(){
-    int num[] = {1, 2, 3, 4, 5,};
-    for(int i = 0; i < 5; i++){
-        printf("%d\n", num[i]);
-    }
-}
+#include<windows.h>
 
 int main(){
 
-    // setlocale(LC_ALL, "pt_BR.UTF8");
-     setlocale(LC_ALL, "Portuguese_Brazil.1252");
-    //SetConsoleOutputCP(65001);
-    //SetConsoleCP(65001);
+    setlocale(LC_ALL, "pt_BR.UTF8");
+    setlocale(LC_ALL, "Portuguese_Brazil.1252");
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
 
-   numeros();
+    int numero;
 
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    while(numero <= 5){
+        printf("Número: %d\n", numero);
+        numero++;
+    }
+    
     return 0;
 }
