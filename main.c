dab12d5311029127e2ab9de972914c8badb2fa6b
profedufbarreto <01289211027@senacrs.edu.br>
2026-09-25 13:30:51 -0300

    contar vogais

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<locale.h>
+#include<string.h>
 //#include<windows.h>
 
-void fibonacci(int termos){
-    int a = 0, b = 1, c;
-    printf("Sequência de Fibonacci (%d termos): \n", termos);
-    for(int i = 1; i <= termos; i++){
-        printf("%d\n", a);
-        c = a + b;
-        a = b;
-        b = c;
+void contarVogais(char palavra[]) {
+    int contador = 0;
+    for (int i = 0; i < strlen(palavra); i++) {
+        char c = palavra[i];
+        if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+            c=='A'||c=='E'||c=='I'||c=='O'||c=='U') {
+            contador++;
+        }
     }
-
-    printf("\n");
+    printf("A palavra '%s' tem %d vogais.\n", palavra, contador);
 }
 
 int main(){
@@ -22,7 +22,12 @@ int main(){
     //SetConsoleOutputCP(65001);
     //SetConsoleCP(65001);
 
-    fibonacci(10);
+    char p[20];
+
+    printf("Digite um palavra: ");
+    scanf("%s", p);
+
+    contarVogais(p);
 
     return 0;
 }
